74fee70b0e4ca00e7a07d9ca3e8db204c30e66ca
Ergothkampfgaist94 <48288286+hygConsultores@users.noreply.github.com>
2023-11-02 10:49:28 -0500

    first commit
    :xq
    :xw

---
+++ src/Objects/Personaje_1.java
@@ -0,0 +1,5 @@
+package Objects;
+
+public class Personaje_1 extends cls_Padre_Personaje {
+    
+}
---
+++ src/proyecto_final/Proyecto_Final.java
@@ -0,0 +1,14 @@
+
+/**
+ *(Ergoth) Andrés Felipe López López
+ * 
+ */
+package proyecto_final;
+
+public class Proyecto_Final {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
