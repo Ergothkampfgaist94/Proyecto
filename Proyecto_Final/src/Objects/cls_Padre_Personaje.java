abd3886ca681f1265a002b767e78272d01cd9979
Ergothkampfgaist94 <67721909+Ergothkampfgaist94@users.noreply.github.com>
2023-11-02 10:29:26 -0500

    Add files via upload

---
+++ Proyecto_Final/src/Objects/cls_Padre_Personaje.java
@@ -0,0 +1,34 @@
+package Objects;
+
+public class cls_Padre_Personaje {
+    
+    private int Health_points;
+    private int Attack;
+    private int Defense;
+
+    public int getHealth_points() {
+        return Health_points;
+    }
+
+    public void setHealth_points(int Health_points) {
+        this.Health_points = Health_points;
+    }
+
+    public int getAttack() {
+        return Attack;
+    }
+
+    public void setAttack(int Attack) {
+        this.Attack = Attack;
+    }
+
+    public int getDefense() {
+        return Defense;
+    }
+
+    public void setDefense(int Defense) {
+        this.Defense = Defense;
+    }
+    
+    
+}
